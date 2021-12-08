d9e5361a4096a0082dd1d5e606132f0106b23e5c
RifkyAl07 <92801053+RifkyAl07@users.noreply.github.com>
2021-12-08 09:44:28 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-# praktikum-5# Praktikum 5
-
-### Program Data Mahasiswa
-membuat data mahasiswa menggunakan Dictionary dengan python.</p>
-NAMA    : Rifky Al Fares</p>
-KELAS   : T1.21.CA.1</p>
-NIM     : 312110477</p>
-UNIVERSITAS PELITA BANGSA BEKASI</p>
-
-#### Source Code
-
-```python
 bot = {}
 
 while True:
@@ -18,9 +6,9 @@ while True:
     print(header.center(97,"="))
     print()
     print(header2.center(97,"_"))
-    c = input("\n(L)ihat, (T)ambah, (U)bah), (H)apus, (C)ari, (K)eluar: ")
+    pilih = input("\n(L)ihat, (T)ambah, (U)bah), (H)apus, (C)ari, (K)eluar: ")
 
-    if c.lower() == 't':
+    if pilih.lower() == 't':
         print("Tambah Data")
         nama = input("Nama\t\t: ")
         nim = int(input("NIM\t\t: "))
@@ -30,7 +18,7 @@ while True:
         akhir = tugas*30/100 + uts*35/100 + uas*35/100
         bot[nama] = nim, uts, uas, tugas, akhir
 
-    elif c.lower() == 'u':
+    elif pilih.lower() == 'u':
         print("Ubah Data")
         nama = input("Masukkan Nama   : ")
         if nama in bot.keys():
@@ -43,7 +31,7 @@ while True:
         else:
             print("Nama {0} tidak ditemukan".format(nama))
 
-    elif c.lower() == 'h':
+    elif pilih.lower() == 'h':
         print("Hapus Data")
         nama = input("Masukkan Nama  : ")
         if nama in bot.keys():
@@ -51,7 +39,7 @@ while True:
         else:
             print("Nama {0} Tidak Ditemukan".format(nama))
 
-    elif c.lower() == 'c':
+    elif pilih.lower() == 'c':
         print("Cari Data[case-sensitive]")
         nama = input("Masukkan Nama : ")
         if nama in bot.keys():
@@ -66,7 +54,7 @@ while True:
         else:
             print("Nama {0} Tidak Ditemukan".format(nama))
 
-    elif c.lower() == 'l':
+    elif pilih.lower() == 'l':
         if bot.items():
             print("="*78)
             print("|                               Daftar Mahasiswa                             |")
@@ -88,47 +76,8 @@ while True:
             print("|                                TIDAK ADA DATA                              |")
             print("="*78)
 
-    elif c. lower() == 'k':
+    elif pilih. lower() == 'k':
         break
 
     else:
         print("Pilih menu yang tersedia")
-
-```
-
-#### Penjelasan :
-
-1.) Pertama kita membuat sebuah dictionary kosong yang nantinya akan diinputkan data ketika program dijalankan.
-```python
-bot = {}
-```
-
-2.) Lalu kita membuat kondisi perulangan dan sebuah keterangan untuk pilihan menu yang akan menjalankan program.
-3.) Membuat syntax untuk menambahkan data.
-Disini apabila kita menginputkan 't' maka kita akan diminta untuk menginputkan beberapa data. Data yang kita inputkan akan masuk ke dictionary 'x' yang telah dibuat tadi dengan data 'nama' sebagai keys dan sisanya sebagai valuesnya.
-
-4.) Membuat syntax untuk mengubah data.
-Apabila kita menginput 'u' maka akan ada keterangan untuk mengubah data dan kita akan diminta untuk menginputkan nama yang mau diubah datanya, apabila nama tidak ada maka outputnya "Nama {} tidak ditemukan". Dimana `{}` adalah nama/data yang mau kita ubah.
-
-5.) Membuat syntax untuk menghapus data.
-
-Apabila kita menginput 'h' maka kita akan diminta menginput nama yang akan dihapus. Jika nama ada di dalam dictionary, maka system akan menghapus keys/nama tersebut beserta valuesnya pada statement `del x[nama]`.
-
-6.) Membuat syntax untuk mencari data.
-
-Apabila kita menginputkan 'c' maka kita akan diminta untuk memasukkan nama yang akan dicari. Apabila nama yang dicari ada di dalam dictionary maka outputnya akan menampilkan data dari nama tersebut.
-
-7.) Membuat syntax untuk melihat atau menampilkan data.
-
-Apabila kita menginput 'l' maka sistem akan menampilkan data - data yang sudah kita masukkan. Jika kita belum memasukkan data maka outputnya menjadi "TIDAK ADA DATA".
-
-8.) Membuat syntax untuk menghentikan perulangan.
-
-Apabila kita menginput 'k' maka program akan langsung berhenti.
-
-9.) Membuat syntax untuk apabila memilih pilihan yang tidak ada di menu.
-
-Jika kita menginputkan selain yang ada pada menu (t, u, h, c, l, k) maka kita akan diminta untuk memilih menu yang tersedia.
-
-
-
